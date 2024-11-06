9a5959d2d39c20826982fe3e9313b05c33e41d63
Sushsnat <grtsushant@gmail.com>
2024-11-06 23:13:18 +0530

    modified css

--- frontend/src/components/WalletComponent.jsx
+++ frontend/src/components/WalletComponent.jsx
@@ -18,10 +18,10 @@ const WalletComponent = () => {
   return (
     <div style={{width: "100%", display:"flex", justifyContent: "center"}}>
       {
-        !wallet ? (<>
+        !wallet ? (<div style={{display: "flex", flexDirection: "column", alignItems:"center", justifyContent: "center"}}>
           <dotlottie-player src="https://lottie.host/50ced29f-5404-4fc8-8a8b-68bf2a714a14/JJ0z0rck4b.json" background="transparent" speed="1" style={{ height: "200px", with: "200px" }} loop autoplay></dotlottie-player>
           <WalletMultiButton />
-        </>)
+        </div>)
           :
           (
               <div className="h" style={{ height: "550px", overflowY: "auto", width: "60%" }}>
